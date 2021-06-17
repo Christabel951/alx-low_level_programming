aa2e45522b8ddbefc5b72ff8a1ef8225e4352541
Christabel951 <crkomo@gmail.com>
2021-06-17 20:44:18 +0000

    Add file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints random specified string followed by new line to standard error
+ * Return: 0
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-1019\n");
+}
