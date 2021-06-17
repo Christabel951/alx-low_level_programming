46034fc35ab890074f14902f8dd25440fb0e7a4c
Christabel951 <crkomo@gmail.com>
2021-06-17 19:22:28 +0000

    Rectify file 4 string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
  * main - Initiates execution of programme file
- * Return: The value 0 to denote success execution
+ *
  * DESCRIPTION: It is the entry point of program
- */
+ * Return: The value 0 to denote successful execution
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
