c8e18aea28f016d2b17338bc058598baa69b5ea1
Christabel951 <crkomo@gmail.com>
2021-06-17 19:13:41 +0000

    Add 3 scripts and 1 c program file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Initiates execution of programme file
+ * Return: The value 0 to denote success execution
+ * DESCRIPTION: It is the entry point of program
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
