9a71e12bf2fa38b6c499369ce23d156890537465
Christabel951 <crkomo@gmail.com>
2021-06-17 20:59:03 +0000

    Rectify 101 file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Prints string
- * Description: Prints random specified string followed by new line to standard error
+ * Description: Prints random specified string
  * Return: 0
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-1019\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
