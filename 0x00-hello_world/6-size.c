2277a3084a88a21702916cb8255db7590ff87748
Christabel951 <crkomo@gmail.com>
2021-06-17 18:02:59 +0000

    Add file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * main - Initiates execution of programme file
+ * Return: The value 0 when the execution is successful. Otherwise display errors
+ */
+int main(void)
+{
+	char char_type;
+	int int_type;
+	long int int_long_type;
+	long long int int_long_long_type;
+	float float_type;
+	printf("Size of a char: %lu byte(s)\n", sizeof(char_type));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int_type));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(int_long_type));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(int_long_long_type));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float_type));
+	return (0);
+}
