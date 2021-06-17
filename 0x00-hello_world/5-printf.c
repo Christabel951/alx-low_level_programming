e41a6441b2ec05524ae2336e65a250aa44377e97
Christabel951 <crkomo@gmail.com>
2021-06-17 16:10:19 +0000

    Add betty fuction doc style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
+	/**
+	 * print f - Prints a string of characters to the standard output
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
