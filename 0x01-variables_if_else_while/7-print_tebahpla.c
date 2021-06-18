606230cb963356c1065dd071222a9a1b85091e84
Christabel951 <crkomo@gmail.com>
2021-06-18 18:09:27 +0000

    Add file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints random specified string
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
