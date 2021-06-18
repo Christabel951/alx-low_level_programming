dcc3631c3e69f98c5e07cf4f374a82a031b309f0
Christabel951 <crkomo@gmail.com>
2021-06-18 15:19:11 +0000

    Add left out variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,10 @@ int main(void)
 	/* your code goes there */
 	last_d = n % 10;
 	if (last_d > 5)
-		printf("Last digit of %d is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	if (last_d == 0)
-		printf("Last digit of %d is and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
 	if ((last_d < 6) && (last_d != 0))
-		printf("Lat digit of %d is and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	return (0);
 }
