2108add839e8b647a217d399f83255a2802efb62
Christabel951 <crkomo@gmail.com>
2021-06-19 15:33:38 +0300

    Add file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints random specified string
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
