874f607ecee360bd56f0f811a89920f0af87f7ca
Christabel951 <crkomo@gmail.com>
2021-06-20 11:47:16 +0300

    Add file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ int main(void)
 
 	for (i = 1; i <= 89; i++)
 	{
-		f_digit = ((i/10) + '0');
-		s_digit = ((i%10) + '0');
-		putchar(f_digit);
-		putchar(s_digit);
-		if (i != 89)
+		f_digit = (i/10);
+		s_digit = (i%10);
+		if ((f_digit != s_digit) && (s_digit != 0) && (f_digit < s_digit))
 		{
-			putchar(44);
-			putchar(32);
+			putchar(f_digit + '0');
+			putchar(s_digit + '0');
+			if (i != 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
 	putchar('\n');
