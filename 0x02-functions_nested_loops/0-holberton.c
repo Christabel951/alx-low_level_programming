f21fcb65fccf74eb28296ecacc12114d482eb594
Christabel951 <crkomo@gmail.com>
2021-06-22 21:44:40 +0300

    Debug betty style space in file 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -12,10 +12,10 @@ _putchar('o');
 _putchar('l');
 _putchar('b');
 _putchar('e');
-    	_putchar('r');
-    	_putchar('t');
-    	_putchar('o');
-    	_putchar('n');
-    	_putchar('\n');
-    	return (0);
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
