930a32e833ddc490f5932cafe548785acdb5b9a6
Christabel951 <crkomo@gmail.com>
2021-06-22 23:15:18 +0300

    Update header file, add file 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print lower case alphabets.
+ * Description: encapsulates the fuction to print lowercase alphabets 10 times
+ * Return: no return value.
+ */
+int _islower(int c)
+{
+if ((c >= 'a') && (c <= 'z'))
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+}
