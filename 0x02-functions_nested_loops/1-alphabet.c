f719d0572ae48fb6d0a488279cd484504a0953db
Christabel951 <crkomo@gmail.com>
2021-06-22 22:39:13 +0300

    Add betty space styling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ * print_alphabet - print lower case alphabets.
+ * Description: encapsulates the fuction to print lowercase alphabets
+ * Return: no return value.
  */
 void print_alphabet(void)
 {
 int i;
 
-for (i = 'a';i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
