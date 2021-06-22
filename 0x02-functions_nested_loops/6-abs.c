d1126966caaaab940e5d0b082c76a74419e20236
Christabel951 <crkomo@gmail.com>
2021-06-23 00:08:41 +0300

    Add file 6, Update header file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute absolute value of interger.
+ * Description: accepts one paramenter which it evaluates set conditions.
+ * @parameter: The parameter is of type int.
+ * Return: value included in if statements
+ */
+int _abs(int)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
