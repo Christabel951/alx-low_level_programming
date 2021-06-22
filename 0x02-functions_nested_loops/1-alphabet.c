ba396d19c52a5f380a09701e86f50c97f67528fe
Christabel951 <crkomo@gmail.com>
2021-06-22 21:59:22 +0300

    Add file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int alpha;
+
+print_alphabet(void);
+_putchar('\n');
+return (0);
+}
