0741788154b17dda41ad4f46316af75384df8744
Christabel951 <crkomo@gmail.com>
2021-06-22 21:41:50 +0300

    Debug betty styles spaces

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    	_putchar('H');
-    	_putchar('o');
-    	_putchar('l');
-    	_putchar('b');
-    	_putchar('e');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
     	_putchar('r');
     	_putchar('t');
     	_putchar('o');
