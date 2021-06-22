340e3d915b68342054be773507b5b2d9c6c498c9
Christabel951 <crkomo@gmail.com>
2021-06-22 23:27:33 +0300

    Add betty doc description for c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - check if argument passed is lowercase alphabets.
- * Description: accepts one paramenter of type int which it evaluates
+ * Description: accepts one paramenter c of type int which it evaluates
  * Return: no return value.
  */
 int _islower(int c)
