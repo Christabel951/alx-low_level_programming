b5aee290cb8e9a6b35f5053d1714ce88d694cc48
Christabel951 <crkomo@gmail.com>
2021-06-23 13:16:30 +0300

    Update header file, add file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers.
+ *
+ * Description:Accepts two arguments of type int 
+ * and returns the computed addition.
+ *
+ * @a:first number in parameter list.
+ * @b:second number in parameter list.
+ *
+ * Return: Has a return value of type int.
+ */
+int add(int a, int b)
+{
+int sum = a + b;
+if (sum >= 0)
+{
+return (sum);
+}
+else 
+{
+return (-sum + 48);
+}
+}
