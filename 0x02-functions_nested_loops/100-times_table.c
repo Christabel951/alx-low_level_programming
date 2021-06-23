bd22d0441719c54542ce1238edba99dc62113c7f
Christabel951 <crkomo@gmail.com>
2021-06-23 20:27:49 +0300

    Debug check4

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 					ld = p % 10;
 					_putchar(fd + 48);
 					_putchar(ld + 48);
-					if ((i != n) && (j != n))
+					if (j != n)
 					{
 						_putchar(44);
 						_putchar(32);
