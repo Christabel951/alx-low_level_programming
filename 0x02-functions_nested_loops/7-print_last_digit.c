2e2fce366af70b119a74b267108046c29d12602e
Christabel951 <crkomo@gmail.com>
2021-06-23 08:24:46 +0300

    Add condition to handle negative numbers in file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,21 @@
 
 /**
  * print_last_digit - compute the last digit of an integer argument.
- * Description: accepts one paramenter which it evaluates to last digit of a number.
+ * Description: accepts one paramenter which it evaluates to last digit.
  * @_ld: The parameter is of type int.
  * Return: value included in if statements
  */
 int print_last_digit(int _ld)
 {
 _ld %= 10;
+if (_ld >= 0)
+{
+_putchar(_ld + '0');
 return (_ld);
+}
+else 
+{
+_putchar(-_ld + '0');
+return (-_ld);
+}
 }
