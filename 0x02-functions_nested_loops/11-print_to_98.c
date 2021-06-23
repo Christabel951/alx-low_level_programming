e940b2894ee890c3d5e473f451750048401f47e7
Christabel951 <crkomo@gmail.com>
2021-06-23 16:13:52 +0300

    Debug file 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,63 +12,21 @@
  */
 void print_to_98(int nat)
 {
-	if ((nat >= 0) || (nat < 1000))
+	int i;
+	if (nat > 98)
 	{
-		if (nat <= 98)
-		{
-			int i;
-			for (i = nat; i <= 98; i++)
-			{	
-				if (nat < 10)
-				{
-					_putchar(nat + 48);
-					_putchar(44);
-					_putchar(32);
-				}
-				if (nat >= 10)
-				{
-					int nat_fd, nat_ld;
-					nat_fd = nat / 10;
-					nat_ld = nat % 10;
-					_putchar(nat_fd + 48);
-					_putchar(nat_ld + 48);
-					if (nat != 98)
-					{
-						_putchar(44);
-						_putchar(32);
-					}
-				}	
-			}	
-		}
-
-		if(nat > 98)
-		{
-			int j;
-			for (j = nat; j	> 98; j--)
-			{
-				if (j < 100)
-				{
-					int nat_fd, nat_ld;
-					nat_fd = nat / 10;
-					nat_ld = nat % 10;
-					_putchar(nat_fd + 48);
-					_putchar(nat_ld + 48);
-				}
-				if (j >= 100)
-				{
-					int nat_fd, nat_temp, nat_sd, nat_ld;
-					nat_ld = j % 10;
-					nat_temp = j / 10;
-					nat_sd = nat_temp % 10;
-					nat_fd = nat_temp / 10;
-					_putchar(nat_fd + 48);
-					_putchar(nat_sd + 48);
-					_putchar(nat_ld + 48);
-					_putchar(44);
-					_putchar(32);
-				}
-			}	
-		}	
+	for (i = nat; i > 98; i--)
+	{
+		printf("%d, ", i);
 	}
-	_putchar('\n');
+	}
+	else if (nat < 98)
+	{
+		for (i = nat; i < 98; i++)
+		{
+		printf("%d, ", i);
+		}
+	}
+	printf("%d\n", n);
+
 }
