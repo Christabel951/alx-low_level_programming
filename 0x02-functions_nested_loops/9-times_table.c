51b52d2091912ed2a1e53dfc513d6dabb4a2ae70
Christabel951 <crkomo@gmail.com>
2021-06-23 10:49:21 +0300

    Add provision for double digit products, file7

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,31 @@
  */
 void times_table(void)
 {
-int num_1, num_2, product;
+int num_1, num_2, product, pfd, psd;
 
 for (num_1 = 0; num_1 <= 9; num_1++)
 {
 for (num_2 = 0; num_2 <= 9; num_2++)
 {
 product = num_1 * num_2;
+if (num_2 == 0)
+{
 _putchar(product + 48);
+}
+else if (product < 10)
+{
 _putchar(44);
 _putchar(32);
+_putchar(product + 48);
+}
+else if (product > 10)
+{
+pfd = product / 10;
+psd = product % 10;
+_putchar(44);
+_putchar(32);
+_putchar(pfd + 48);
+_putchar(psd + 48);
 }
 _putchar('\n');
-}
 }
