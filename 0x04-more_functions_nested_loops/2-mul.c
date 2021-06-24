3a0991f7d98a166afd940427d37a4949bfcc3225
Christabel951 <crkomo@gmail.com>
2021-06-24 22:07:18 +0300

    Update headerfile, add file 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - checks for uppercase characters.
+ *
+ * @c: accept 1 parameter of type int.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int p;
+
+p = a * b;
+return (p);
+}
