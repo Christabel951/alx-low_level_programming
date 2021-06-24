e6d800fbc696239604a88a18fe50b63b9263d3aa
Christabel951 <crkomo@gmail.com>
2021-06-24 23:23:51 +0300

    update header file, add file 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 0 - 14 using 3 _putchar.
+ *
+ * @void: Does not accept arguments.
+ *
+ * Return: void - has no return value.
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 14; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+_putchar((j / 10) + '0');
+}
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
+}
