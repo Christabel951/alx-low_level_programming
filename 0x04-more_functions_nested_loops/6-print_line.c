9ebf263a8d2e5a91967a75571be945e0dacee729
Christabel951 <crkomo@gmail.com>
2021-06-24 23:51:12 +0300

    Add file 6, update header

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - prints a line using _ character.
+ *
+ * @n: Accept 1 argument of type int.
+ *
+ * Return: void - has no return value.
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
+}
