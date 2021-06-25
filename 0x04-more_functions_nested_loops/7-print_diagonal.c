32a599512441d32ccf6557227eca7574d95803de
Christabel951 <crkomo@gmail.com>
2021-06-25 11:06:11 +0300

    update header file, add file7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints a diagonal line using \ character.
+ *
+ * @n: Accept 1 argument of type int.
+ *
+ * Return: void - has no return value.
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j<= i; j++)
+{
+if (i == j)
+{
+_putchar(92);
+_putchar('\n');
+}
+else
+{
+_putchar(32);
+}
+}
+}
+}
+else
+{
+_putchar('\n');
+}
+}
