5b9d4272f7cb86562b9beedd14fc7daa1e86a053
Christabel951 <crkomo@gmail.com>
2021-06-28 18:58:55 +0000

    update header file, add file 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap parameter values.
+ *
+ * @a: pointer to an int.
+ * @b: pointer to an int.
+ *
+ * Return: void, nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int h;
+
+	h = *a;
+	*a = *b;
+	*b = h;
+
+}
