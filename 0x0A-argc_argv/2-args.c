1c3bb9d799b3752d66e1c6707a30cd379bf706f3
Christabel951 <crkomo@gmail.com>
2021-07-12 23:00:01 +0300

    add file2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - print program name.
+ * @argc: argument(s) count.
+ * @argv: passed string value.
+ * Return: 0 -exit success.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
