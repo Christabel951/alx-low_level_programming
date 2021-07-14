28b3b2b320c3d9a0788bebbd556483dd4d79867a
Christabel951 <crkomo@gmail.com>
2021-07-14 12:58:29 +0300

    reduce function lines

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+int get_coin_change(int change);
 /**
  * main - print min number of coins to make change for an amount of money.
  * [1, 2, 5, 10, 25]
@@ -11,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int change = 0;
-	int i, count = 0;
+	int changecombn = 0;
 
 	if (argc != 2)
 	{
@@ -27,38 +28,50 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			i = change;
-			while (i >= 1)
-			{
-				if (i >= 25)
-				{
-					change = i / 25;
-					i %= 25;
-				}
-				else if (i >= 10 && i < 25)
-				{
-					change = i / 10;
-					i %= 10;
-				}
-				else if (i >= 5 && i < 10)
-				{
-					change = i / 5;
-					i %= 5;
-				}
-				else if (i >= 2 && i < 5)
-				{
-					change = i / 2;
-					i %= 2;
-				}
-				else
-				{
-					change = i / 1;
-					i %= 1;
-				}
-				count += change;
-			}
-			printf("%d\n", count);
+			changecombn = get_coin_change(change);
+			printf("%d\n", changecombn);
 		}
 		return (0);
 	}
+}
+/**
+ * get_coin_change - get least combination of change.
+ * @_change: balance.
+ * Return: least combination of coint change in int type.
+ */
+int get_coin_change(int change)
+{
+	int i, count = 0;
+
+	i = change;
+	while (i >= 1)
+	{
+		if (i >= 25)
+		{
+			change = i / 25;
+			i %= 25;
+		}
+		else if (i >= 10 && i < 25)
+		{
+			change = i / 10;
+			i %= 10;
+		}
+		else if (i >= 5 && i < 10)
+		{
+			change = i / 5;
+			i %= 5;
+		}
+		else if (i >= 2 && i < 5)
+		{
+			change = i / 2;
+			i %= 2;
+		}
+		else
+		{
+			change = i / 1;
+			i %= 1;
+		}
+		count += change;
+	}
+	return (count);
 }
