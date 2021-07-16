efa3e359e990d77ed71148a6568efaff4af15d47
Christabel951 <crkomo@gmail.com>
2021-07-16 18:06:21 +0300

    add file2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	int *ptr;
+	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +17,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
-	return ((void *)ptr);
+		*(int *)ptr = 0;
+	return (ptr);
 }
