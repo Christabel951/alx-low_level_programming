85bd0a9e91cb691d36545fe279bde1fbe76daa0c
Christabel951 <crkomo@gmail.com>
2021-07-16 21:30:30 +0300

    debug betty space

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,42 +6,42 @@
  * malloc : malloc(old_size).
  * @old_size: size in bytes for the allocated space for ptr.
  * @new_size: size of new mem block in bytes.
- * Return: nothing.
+ * Return: nothing -void.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned char *ptr1;
-	unsigned int i;
+unsigned char *ptr1;
+unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(sizeof(void *) * new_size);
-		if (ptr == NULL)
-			return (NULL);
-		return (ptr);
-	}
-	ptr1 = malloc(sizeof(char) * new_size);
-	if (ptr1 == NULL)
-		return (NULL);
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = ((char *)ptr)[i];
-		free(ptr);
-		return (ptr1);
-	}
-	else
-	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = ((char *)ptr)[i];
-		free(ptr);
-		return(ptr1);
-	}
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+ptr = malloc(sizeof(void *) * new_size);
+if (ptr == NULL)
+return (NULL);
+return (ptr);
+}
+ptr1 = malloc(sizeof(char) * new_size);
+if (ptr1 == NULL)
+return (NULL);
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+ptr1[i] = ((char *)ptr)[i];
+free(ptr);
+return (ptr1);
+}
+else
+{
+for (i = 0; i < new_size; i++)
+ptr1[i] = ((char *)ptr)[i];
+free(ptr);
+return(ptr1);
+}
 }
