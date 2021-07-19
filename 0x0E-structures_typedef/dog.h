b50de1db8a1c058fcfbd1d426fddb068f67cd2b4
Christabel951 <crkomo@gmail.com>
2021-07-20 00:55:44 +0300

    add betty docs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - structure dog declaration
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ */
 struct dog
 {
 	char *name;
