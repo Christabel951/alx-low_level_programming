f3870742bc03d2a2f9bce522c94c376da0ef4574
Christabel951 <crkomo@gmail.com>
2021-07-20 22:02:47 +0300

    allocate more mem

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,46 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strlen - get length of string.
+ * @s: pointer to a char.
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
 
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
+}
+/**
+ * _strcpy - copies the string including \0 to buffer pointed to.
+ *
+ * @dest: pointer where string is copied.
+ * @src: string arg to be copied.
+ *
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int sl = 0;
+
+	while (src[sl] != '\0')
+	{
+		sl++;
+	}
+	for (i = 0; i < sl; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * new_dog - create new dog
  * @name: pointer to type char
@@ -12,15 +51,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndg;
+	int len = 0, leno = 0;
 
+	len = _strlen(name);
+	leno = _strlen(owner);
 	ndg = malloc(sizeof(dog_t));
 	if (ndg == NULL)
 		return (NULL);
-	else
+	ndg->name = malloc(sizeof(char) * len + 1);
+	if (ndg->name == NULL)
 	{
-		ndg->name= name;
-		ndg->age = age;
-		ndg->owner = owner;
-		return (ndg);
+		free(ndg);
+		return (NULL);
 	}
+	ndg->owner = malloc(sizeof(char) * leno + 1);
+	if (ndg->owner == NULL)
+	{
+		free(ndg);
+		return (NULL);
+	}
+	_strcpy(ndg->name, name);
+	_strcpy(ndg->owner, owner);
+	ndg->age = age;
+	return (ndg);
 }
