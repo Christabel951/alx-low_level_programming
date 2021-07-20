1b2cce8b863352e065fb647aa4acc8017b16e0d1
Christabel951 <crkomo@gmail.com>
2021-07-20 18:49:29 +0300

    add file5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - free dogs
+ * @d: pointer to type dog_t
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	else
+		free(d);
+}
