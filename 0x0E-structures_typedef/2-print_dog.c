f25cb8aed40c9da61f7d17a28d8185db9aa54428
Christabel951 <crkomo@gmail.com>
2021-07-20 15:10:47 +0300

    add if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		printf("Name: (nill)\n");
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
-		printf("Name: (nill)\n");
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nill)\n");
+		printf("Name: %s\n", d->name);
+		printf("Age: %.6f\n", d->age);
+		if (d->owner == NULL)
+			printf("Name: (nill)\n");
+		printf("Owner: %s\n", d->owner);
+	}
 }
