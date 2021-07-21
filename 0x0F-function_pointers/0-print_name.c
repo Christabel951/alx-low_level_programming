cb5c015bf569d18d21d36e1e0f27a892f34c0019
Christabel951 <crkomo@gmail.com>
2021-07-22 00:26:46 +0300

    add betty docs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,10 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- *
- *
+ *print_name - print name using function pointer as argument
+ *@name: string value
+ *@f: function pointer
+ *Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
