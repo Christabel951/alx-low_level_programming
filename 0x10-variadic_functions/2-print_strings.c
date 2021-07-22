923d07ed7bd990164b3111bff77ae2538299f40e
Christabel951 <crkomo@gmail.com>
2021-07-22 23:26:25 +0300

    debug if statements

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			s = va_arg(str, char *);
-			if (s == NULL || *s == '\0')
+			if (s == NULL)
 				printf("(nil)");
 			else
 			{
