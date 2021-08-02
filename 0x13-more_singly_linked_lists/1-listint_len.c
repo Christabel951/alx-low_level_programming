e7965642d22460b174f96a414445802980d112b1
Christabel951 <crkomo@gmail.com>
2021-08-02 15:39:47 +0300

    Add file1, update .h

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/** print_listint - return the number of elements in listint_t list
+ * @h: pointer variable containing 1st node address
+ *
+ * Return: number of nodes in link
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
+
+
