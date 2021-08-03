c33150631f51875b91b1cdaccfcb10c82b5fced6
Christabel951 <crkomo@gmail.com>
2021-08-03 17:49:11 +0300

    debug betty spaces

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,24 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_listint - free list
+ * free_listint2 - free list
  * @head: pointer to pointer variable containing 1st node's address
  *
  * Description: delete list and initializes *head to NULL
  */
 void free_listint2(listint_t **head)
 {
-        listint_t *temphead;
+	listint_t *temphead;
 	listint_t *temp;
 
-        if (!head)
-                return;
+	if (!head)
+		return;
 	temphead = *head;
-        while (temphead)
-        {
+	while (temphead)
+	{
 		temp = temphead->next;
 		free(temphead);
-                temphead = temp;
-        }
+		temphead = temp;
+	}
 	*head = NULL;
 }
