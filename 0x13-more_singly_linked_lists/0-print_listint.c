fce37e02309f5035da4ff0a21d4937ac06f818cf
Christabel951 <crkomo@gmail.com>
2021-08-03 10:20:32 +0300

    add new line if NULL

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,16 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-
-	while (h)
+	if (!h)
+		printf("\n");
+	else
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		num++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			num++;
+		}
 	}
 	return (num);
 }
