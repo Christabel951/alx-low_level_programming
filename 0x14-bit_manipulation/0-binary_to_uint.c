78f37f9df1147778c747802441a2ae00e2868e14
Christabel951 <crkomo@gmail.com>
2021-08-07 23:02:57 +0300

    add file0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the length of a string
+ * @s: pointer to a char
+ *
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int counter = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
+}
+/**
+ * _pow_recursion - print result of value x raised to power y.
+ * @x:value of number.
+ * @y: power value.
+ *
+ * Return: integer value - result.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned integer
+ * @b: binary value in string
+ *
+ * Description: Transforms a binary number represented in string value
+ * into an unsigned integer value
+ * Return: unsigned integer value if success, else 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, j, convert, flag, result = 0;
+	const unsigned int base = 2;
+	unsigned int len = _strlen(b);
+
+	if (!b)
+		return (0);
+	j = len - 1;
+	for (i = 0; i < len; i++)
+	{
+		if(b[i] == '1')
+			flag = 1;
+		else if (b[i] == '0')
+			flag = 0;
+		else
+			return (0);
+		convert = flag * _pow_recursion(base, j);
+		result += convert;
+		j--;
+	}
+	return (result);
+}
