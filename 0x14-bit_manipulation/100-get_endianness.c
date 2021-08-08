6c9eaaf05709fe4826f7ddfa86d5fd0586f23a25
Christabel951 <crkomo@gmail.com>
2021-08-08 22:40:58 +0300

    add file 100, update .h

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - check endianness
+ *
+ * Description: get the type of endiannes used for storing
+ * multibyte data-type [little endian and big endian]
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char*) &i;
+
+	return (*c);
+}
